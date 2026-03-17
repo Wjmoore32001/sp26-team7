f4b693c3a2397cec1603da96398da71d40d98a4a
William Moore <WilliamJMooreIII@proton.me>
2026-03-17 13:17:19 -0400

    spring initilzr

---
+++ backend-api/src/test/java/edu/UNCG/sp26team7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.UNCG.sp26team7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
