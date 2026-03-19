e48dd461bfda83dbb3abb1cd58ef2e078f955697
William Moore <WilliamJMooreIII@proton.me>
2026-03-19 11:30:44 -0400

    Completed most of ClassTemplate entity

--- backend-api/src/main/java/edu/UNCG/sp26team7/entity/ClassTemplate.java
+++ backend-api/src/main/java/edu/UNCG/sp26team7/entity/ClassTemplate.java
@@ -1,12 +1,21 @@
 
 package edu.UNCG.sp26team7.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import edu.UNCG.sp26team7.entity.enums.ClassTemplateType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,10 +36,40 @@ public class ClassTemplate {
   @JoinColumn(name = "instructor_id")
   private Instructor instructor;
 
+  @Column
   private String title;
 
+  @Column
+  @Enumerated(EnumType.STRING)
   private ClassTemplateType classType;
 
+  @Column
+  @Enumerated(EnumType.STRING)
+  private IntensityLevels intensity;
+
+  @Column
   private int duration;
 
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Column(nullable = false, precision = 6, scale = 2)
+  private BigDecimal price;
+
+  @Column
+  private String description;
+
+  @PrePersist
+  private void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  private void onUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
 }
