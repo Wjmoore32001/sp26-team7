96117d8c0fa8f6740041c6fdf57f0d7927e0ffbf
Chloe Rhodes <carhodes@uncg.edu>
2026-03-18 21:50:00 -0400

    finished User.java

--- backend-api/src/main/java/edu/UNCG/sp26team7/entity/User.java
+++ backend-api/src/main/java/edu/UNCG/sp26team7/entity/User.java
@@ -1,5 +1,62 @@
 package edu.UNCG.sp26team7.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
-    
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String passwordHash;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private UserRole role;
+
+  public User() {}
+
+  public Long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Long userId) {
+    this.userId = userId;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPasswordHash() {
+    return passwordHash;
+  }
+
+  public void setPasswordHash(String passwordHash) {
+    this.passwordHash = passwordHash;
+  }
+
+  public UserRole getRole() {
+    return role;
+  }
+
+  public void setRole(UserRole role) {
+    this.role = role;
+  }
+
+}
+
+enum UserRole {
+  STUDENT,
+  INSTRUCTOR
 }
