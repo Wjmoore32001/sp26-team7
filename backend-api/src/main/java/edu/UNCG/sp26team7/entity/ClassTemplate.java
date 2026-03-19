eda2980076bf1b0b6f483674f7243cc35984e482
William Moore <WilliamJMooreIII@proton.me>
2026-03-19 09:28:31 -0400

    added category enum and some fields for ClassSession

--- backend-api/src/main/java/edu/UNCG/sp26team7/entity/ClassTemplate.java
+++ backend-api/src/main/java/edu/UNCG/sp26team7/entity/ClassTemplate.java
@@ -2,13 +2,22 @@
 package edu.UNCG.sp26team7.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ClassTemplates")
 @Data
 @NoArgsConstructor
-@All
+@AllArgsConstructor
 public class ClassTemplate {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long template_id;
 }
---
+++ backend-api/src/main/java/edu/UNCG/sp26team7/entity/ClassTemplateCategory.java
@@ -0,0 +1,11 @@
+package edu.UNCG.sp26team7.entity;
+
+public enum ClassTemplateCategory {
+  WEIGHTLIFTING,
+  ZUMBA,
+  YOGA,
+  PILATES,
+  CARDIO,
+  CROSSFIT
+
+}
