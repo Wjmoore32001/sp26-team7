94a5af0948db696787f0b823d1e706bf48c90419
William Moore <WilliamJMooreIII@proton.me>
2026-03-19 08:38:59 -0400

    Added dependencies and linux permission for running mvnw as a script

---
+++ backend-api/src/main/java/edu/UNCG/sp26team7/entity/ClassTemplate.java
@@ -0,0 +1,14 @@
+
+package edu.UNCG.sp26team7.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ClassTemplates")
+@Data
+@NoArgsConstructor
+@All
+public class ClassTemplate {
+
+}
