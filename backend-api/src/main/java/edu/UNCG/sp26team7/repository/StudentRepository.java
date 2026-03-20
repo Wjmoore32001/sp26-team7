2ce512d7329fc34788c13d6a234fa7aaca32e5c6
Chloe Rhodes <carhodes@uncg.edu>
2026-03-19 21:33:25 -0400

    Completed the repositorys

--- backend-api/src/main/java/edu/UNCG/sp26team7/repository/StudentRepository.java
+++ backend-api/src/main/java/edu/UNCG/sp26team7/repository/StudentRepository.java
@@ -1,5 +1,12 @@
 package edu.UNCG.sp26team7.repository;
 
-public class StudentRepository {
-    
+import edu.UNCG.sp26team7.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Student findByEmail(String email);
+
 }
--- backend-api/src/main/java/edu/UNCG/sp26team7/repository/StudentScheduleRepository.java
+++ backend-api/src/main/java/edu/UNCG/sp26team7/repository/StudentScheduleRepository.java
@@ -1,5 +1,14 @@
 package edu.UNCG.sp26team7.repository;
 
-public class StudentScheduleRepository {
-    
+import edu.UNCG.sp26team7.entity.StudentSchedule;
+import edu.UNCG.sp26team7.entity.Student;
+import edu.UNCG.sp26team7.entity.ClassSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentScheduleRepository extends JpaRepository<StudentSchedule, Long> {
+
+    boolean existsByStudentAndClassSession(Student student, ClassSession classSession);
+
 }
