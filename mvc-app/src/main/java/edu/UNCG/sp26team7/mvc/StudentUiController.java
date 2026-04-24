26621817e87ede787cf0d3f97263d364b9b1fb4e
Chloe Rhodes <carhodes@uncg.edu>
2026-04-24 16:06:38 -0400

    Added mapping for enrolling

--- mvc-app/src/main/java/edu/UNCG/sp26team7/mvc/StudentUiController.java
+++ mvc-app/src/main/java/edu/UNCG/sp26team7/mvc/StudentUiController.java
@@ -1,5 +1,7 @@
 package edu.UNCG.sp26team7.mvc;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.UNCG.sp26team7.service.ClassSessionService;
+import edu.UNCG.sp26team7.service.StudentScheduleService;
 import edu.UNCG.sp26team7.service.StudentService;
+import edu.UNCG.sp26team7.entity.ClassSession;
 import edu.UNCG.sp26team7.entity.Student;
+import edu.UNCG.sp26team7.entity.StudentSchedule;
+import edu.UNCG.sp26team7.entity.enums.BookingStatus;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -22,6 +28,9 @@ public class StudentUiController {
 
     @Autowired
     private ClassSessionService classSessionService;
+
+    @Autowired
+    private StudentScheduleService studentScheduleService;
 
     @GetMapping("/profile")
     public String getProfile(HttpSession session, Model model) {
@@ -81,6 +90,29 @@ public class StudentUiController {
     @GetMapping("/home")
     public String home() {
         return "student/home";
+    }
+
+    @PostMapping("/enroll")
+    public String enroll(@RequestParam Long classSessionId, HttpSession session) {
+        Long studentId = (Long) session.getAttribute("studentId");
+
+        if (studentId == null) {
+            return "redirect:/signin";
+        }
+
+        StudentSchedule schedule = new StudentSchedule();
+        Student student = new Student();
+        student.setUserId(studentId);
+        ClassSession classSession = new ClassSession();
+        classSession.setClassSessionId(classSessionId);
+
+        schedule.setStudent(student);
+        schedule.setClassSession(classSession);
+        schedule.setEnrolledAt(LocalDateTime.now());
+        schedule.setBookingStatus(BookingStatus.ENROLLED);
+
+        studentScheduleService.createStudentSchedule(schedule);
+        return "redirect:/student/browse?joined";
     }
 
     @GetMapping("/logout")
