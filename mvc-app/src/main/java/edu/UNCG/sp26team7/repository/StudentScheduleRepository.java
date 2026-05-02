1bd5da5690d821cc377bf762d812511e81ae3b2f
William Moore <WilliamJMooreIII@proton.me>
2026-05-02 15:00:13 -0400

    delete student schedules before deleting session

--- mvc-app/src/main/java/edu/UNCG/sp26team7/repository/StudentScheduleRepository.java
+++ mvc-app/src/main/java/edu/UNCG/sp26team7/repository/StudentScheduleRepository.java
@@ -1,19 +1,18 @@
 package edu.UNCG.sp26team7.repository;
 
-import edu.UNCG.sp26team7.entity.StudentSchedule;
-import edu.UNCG.sp26team7.entity.Student;
 import edu.UNCG.sp26team7.entity.ClassSession;
-
-import java.util.List;
-
+import edu.UNCG.sp26team7.entity.Student;
+import edu.UNCG.sp26team7.entity.StudentSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentScheduleRepository extends JpaRepository<StudentSchedule, Long> {
-
   boolean existsByStudentAndClassSession(Student student, ClassSession classSession);
 
   List<StudentSchedule> findByStudentUserId(Long userId);
 
+  void deleteByClassSessionClassSessionId(Long classSessionId);
 }
