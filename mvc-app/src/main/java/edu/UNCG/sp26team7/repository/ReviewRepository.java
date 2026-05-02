3c43415136d506deb607f1471f9a5e39eb2b5bdc
William Moore <WilliamJMooreIII@proton.me>
2026-05-02 15:13:13 -0400

    Added template review delete helper

--- mvc-app/src/main/java/edu/UNCG/sp26team7/repository/ReviewRepository.java
+++ mvc-app/src/main/java/edu/UNCG/sp26team7/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package edu.UNCG.sp26team7.repository;
 
 import java.util.List;
+
 import edu.UNCG.sp26team7.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
   @Query(value = "SELECT r.* FROM reviews r WHERE r.class_template_id = :classTemplateId", nativeQuery = true)
   List<Review> findByClassTemplateId(Long classTemplateId);
 
+  void deleteByClassTemplateClassTemplateId(Long classTemplateId);
 }
